9023b5428d889631a675cde9b6b53471504e3850
computerscistudent <abhimanyustomar24@gmail.com>
2025-11-23 16:36:19 +0530

    Upgraded to semantic resume matcher

--- src/api/main.py
+++ src/api/main.py
@@ -9,6 +9,33 @@ import pandas as pd, pdfplumber, docx, io, re, string, os
 app = FastAPI(title="ATS Resume Ranking Portal")
 templates = Jinja2Templates(directory="src/api/templates")
 
+from sentence_transformers import SentenceTransformer, util
+import os
+
+os.environ["TRANSFORMERS_CACHE"] = "/tmp/cache"
+
+print("🚀 Loading SentenceTransformer model...")
+
+model = SentenceTransformer(
+    "sentence-transformers/all-MiniLM-L6-v2",
+    cache_folder="/tmp/cache"  
+)
+
+model.encode("warmup test", convert_to_tensor=True)
+
+print("✅ Model loaded and warmed up!")
+# -----------------------------
+
+def semantic_score(resume_text, job_text):
+    emb_resume = model.encode(resume_text, convert_to_tensor=True)
+    emb_job = model.encode(job_text, convert_to_tensor=True)
+    
+    score = util.cos_sim(emb_resume, emb_job).item()
+    score_percentage = (score + 1) / 2 * 100   # Normalize to 0-100
+    
+    return round(score_percentage, 2)
+
+
 
 def clean_text(text):
     text = text.lower()
@@ -65,15 +92,27 @@ async def rank_resumes(request: Request, job_text: str = Form(...), files: list[
         df = pd.DataFrame(resumes)
         job_clean = clean_text(job_text)
 
-        vectorizer = TfidfVectorizer(max_features=3000)
-        vectorizer.fit([job_clean])
+        # use this code if u want a normal resume ranker that only ranks the resume based on text similarity without semantics and meaning getting involved.
+        # -----------------------------------------------------
+        # vectorizer = TfidfVectorizer(max_features=3000)
+        # vectorizer.fit([job_clean])
 
-        job_vec = vectorizer.transform([job_clean]).toarray()
-        resume_vecs = vectorizer.transform(df["text"]).toarray()
+        # job_vec = vectorizer.transform([job_clean]).toarray()
+        # resume_vecs = vectorizer.transform(df["text"]).toarray()
 
-        sims = cosine_similarity(job_vec, resume_vecs)[0] * 100
-        df["similarity"] = sims.round(2)
+        # sims = cosine_similarity(job_vec, resume_vecs)[0] * 100
+        # df["similarity"] = sims.round(2)
+        # -----------------------------------------------------
+
+        # use this if u want all round precise resume ranker which takes in account of semantics , similarity and overall gives u a realible result.
+        # ----------------------------------------------------- 
+        scores = []
+        for text in df["text"]:
+            score = semantic_score(text,job_clean)
+            scores.append(score)
+        df["similarity"] = scores    
         df = df.sort_values(by="similarity", ascending=False)
+        # -----------------------------------------------------
 
         table_html = df[["filename", "similarity"]].to_html(classes="styled-table", index=False)
         return templates.TemplateResponse("result_rank.html", {"request": request, "table": table_html})
@@ -92,14 +131,22 @@ async def upload_resume(request: Request, resume_file: UploadFile = File(...), j
         job_clean = clean_text(job_text)
         resume_clean = clean_text(resume_text)
 
-        vectorizer = TfidfVectorizer(max_features=3000)
-        vectorizer.fit([job_clean])
+        # use this code if u want a normal resume ranker that only ranks the resume based on text similarity without semantics and meaning getting involved.
+        # -----------------------------------------------------
+        # vectorizer = TfidfVectorizer(max_features=3000)
+        # vectorizer.fit([job_clean])
 
-        job_vec = vectorizer.transform([job_clean]).toarray()
-        resume_vec = vectorizer.transform([resume_clean]).toarray()
+        # job_vec = vectorizer.transform([job_clean]).toarray()
+        # resume_vec = vectorizer.transform([resume_clean]).toarray()
 
-        score = cosine_similarity(job_vec, resume_vec)[0][0] * 100
-        score = round(score, 2)
+        # score = cosine_similarity(job_vec, resume_vec)[0][0] * 100
+        # score = round(score, 2)
+        # -----------------------------------------------------
+
+        # use this if u want all round precise resume ranker which takes in account of semantics , similarity and overall gives u a realible result.
+        # ----------------------------------------------------- 
+        score = semantic_score(resume_clean, job_clean)
+        # -----------------------------------------------------
 
         return templates.TemplateResponse("result_score.html", {"request": request, "score": score})
     except Exception as e:
